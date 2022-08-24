775717defe792602955f0a3465245b472c4dfce6
RangoSeto <hsuphyowai.mtla@gmail.com>
2022-08-24 17:09:06 +0630

    partnership

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // Start Header Section
+    // $(window).scroll(function(){
+    //     var getscrolltop = $(this).scrollTop();
+    //     // console.log(getscrolltop);
+
+    //     if(getscrolltop > 287){
+    //         $(".fquotebar").css('position','fixed');
+    //     }
+    // });
+    // End Header Section
+});
