03c95560b03c5b07a3393f10285917ac2d90021e
RangoSeto <hsuphyowai.mtla@gmail.com>
2022-08-25 23:02:45 +0630

    version1

--- app.js
+++ app.js
@@ -9,4 +9,30 @@ $(document).ready(function(){
     //     }
     // });
     // End Header Section
+
+    $("#lightSlider").lightSlider({
+        item: 5,
+        slideMove: 1,
+        slideMargin: 100,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            }
+        ]
+
+    });
+
+    $("#testimonials").lightSlider();
 });
